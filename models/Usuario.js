e58937e528603a3e00b91c5df69e1a4a58658603
Sebastian Vargas <vargaspizango1@gmail.com>
2021-05-30 23:12:52 -0500

    feat: user schema added

---
+++ models/Usuario.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require("mongoose");
+
+const UsuarioSchema = Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+});
+
+module.exports = model('Usuario', UsuarioSchema);
