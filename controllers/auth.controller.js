b1e22e2782fb35940fcf56598e0b4c217374c2a4
Sebastian Vargas <vargaspizango1@gmail.com>
2021-05-30 19:21:56 -0500

    feat: validation fields added when creating account

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,13 @@ const { response } = require("express");
 const { validationResult } = require("express-validator");
 
 const crearUsuario = (req, res = response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            ok: false,
+            error: errors.mapped(),
+        })
+    }
     const { email, name, password } = req.body;
     console.log(email, name, password);
 
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,11 +1,17 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth.controller');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
 // Se crea un nuevo usuario
-router.post('/new', crearUsuario);
+router.post('/new', [
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'La contraseña es obligatoria').isLength({ min: 6 }),
+    validarCampos
+], crearUsuario);
 
 // Login de usuario
 router.post('/', [
